82529968a92a7b81cd397507b4ce8471c6e68e6a
Gerardo0112 <gerar0112@gmail.com>
2020-05-05 22:12:12 -0600

    Codificador en LZW.

--- PROYECTO_ED2/COMPRESION_LZW/LZW.cs
+++ PROYECTO_ED2/COMPRESION_LZW/LZW.cs
@@ -141,5 +141,30 @@ namespace PROYECTO_ED2.COMPRESION_LZW
             TablaComprimir newElement = new TablaComprimir(t, path);
             CheckExistence.Exist.tabla.Insert(0, newElement);
         }
+        //Permite codificar.
+        public string coder(byte[] bytes, ref int x, ref bool r2, ref string r)
+        {
+            string code = string.Empty;
+            if (r2)
+            {
+                code += r;
+            }
+            r2 = false;
+            x++;
+            for (; x < bytes.Length; x++)
+            {
+                if (int.TryParse(((char)bytes[x]).ToString(), out int n))
+                {
+                    code += (char)bytes[x];
+                }
+                else if (x == bytes.Length - 1)
+                {
+                    r2 = true;
+                    r = code;
+                }
+                break;
+            }
+            return code;
+        }
     }
 }
