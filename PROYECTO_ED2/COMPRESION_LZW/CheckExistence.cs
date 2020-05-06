9442d17a2a12796f388d1d0c118e90059b64349b
Gerardo0112 <gerar0112@gmail.com>
2020-05-05 20:29:29 -0600

    Creacion de carpetas, clase para LZW.

---
+++ PROYECTO_ED2/COMPRESION_LZW/CheckExistence.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PROYECTO_ED2.COMPRESION_LZW
+{
+    public class CheckExistence
+    {
+    }
+}
