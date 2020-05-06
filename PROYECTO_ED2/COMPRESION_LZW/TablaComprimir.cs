5877013e644569f8d73fa157356b3ef8f766bcea
Gerardo0112 <gerar0112@gmail.com>
2020-05-05 22:14:40 -0600

    Manejo de tabla de compresiones.

--- PROYECTO_ED2/COMPRESION_LZW/TablaComprimir.cs
+++ PROYECTO_ED2/COMPRESION_LZW/TablaComprimir.cs
@@ -7,6 +7,12 @@ namespace PROYECTO_ED2.COMPRESION_LZW
 {
     public class TablaComprimir
     {
-
+        public string path { get; set; }
+        public string name { get; set; }
+        public TablaComprimir(string t, string path)
+        {
+            name = t;
+            this.path = path;
+        }
     }
 }
